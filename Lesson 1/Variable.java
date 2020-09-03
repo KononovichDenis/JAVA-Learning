8f9a5a6ea4fff869bcab08faa1a354c7e65742d9
Kononovich Denis <denis0071@ya.ru>
2020-09-03 16:22:40 +0300

    Дз1 Ч1

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable{
+	public static void main(String[] args) {
+		byte cpucore = 4;
+		short hddfree = 464;
+		int ram = 8;
+		long hddused = 677824307;
+		float pi = 3.14f;
+		double corespeed = 3.3000d; 
+		char c = 'i';
+		boolean fan = true;
+		System.out.println("Переменная cpucore (byte) = " + cpucore);
+		System.out.println("Переменная hddfree (short) = " + hddfree);
+		System.out.println("Переменная ram (int) = " + ram);
+		System.out.println("Переменная hddused (long) = " + hddused);
+		System.out.println("Переменная pi (float) = " + pi);
+		System.out.println("Переменная corespeed (double) = " + corespeed);
+		System.out.println("Переменная c (char) = " + c);
+		System.out.println("Переменная fan (boolean) = " + fan);
+	}
+}
